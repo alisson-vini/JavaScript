ce27b2c20c6c3662f6386709b8cc528da59b7581
Alisson <alissonvini075@gmail.com>
2025-08-26 16:48:00 -0300

    adicao da pasta introducao_js e seus arquivos

---
+++ introducao_js/index.js
@@ -0,0 +1,7 @@
+window.alert("Essa parte é externa, veja no código") // alerta na tela externo
+
+document.getElementById("texto").innerHTML = "Parte feita com um innerHTML, olher no codigo para entender melhor" // coloca um bloco de HTML por meio de codigo
+
+console.log("mensagem") // coloca informações no console do Devtools
+console.info("informação")
+console.warn("aviso") // é visualmente diferente
